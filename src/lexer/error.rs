505b891d14bc55b13a336978ec3b5445fede0e5d
y_mc <momo.toure27@gmail.com>
2024-08-08 22:08:30 +0200

    rewriting the lexxer

--- src/lexer/error.rs
+++ src/lexer/error.rs
@@ -1,5 +1,19 @@
 use std::fmt;
 use std::fmt::Formatter;
+use std::error::Error;
+
+
+
+/// Enumeration des erreurs du compilateur
+////////////////////////ENUM COMPILER ERROR POUR UTILISATION ULTERIEURE /////////////////////////
+#[derive(Debug)]
+pub enum CompilerError {
+    Lexer(LexerError),
+    // ParserError,
+    // SemanticError,
+    // CodegenError,
+}
+
 
 #[derive(Debug)]
 pub struct LexerError {
@@ -19,21 +33,68 @@ pub enum LexerErrorKind {
 
 }
 
-
-
-
+/// Implementation du trait Display pour LexerError
 impl fmt::Display for LexerError {
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
         write!(f, "LexerError: {} at line {} column {}", self.message, self.line, self.column)
-
     }
-    // fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-    //     match self {
-    //         LexerError::InvalidCharacter(c) => write!(f, "Invalid character: {}", c),
-    //         LexerError::InvalidToken(t) => write!(f, "Invalid token: {}", t),
-    //         LexerError::UnterminatedString => write!(f, "Unterminated string"),
-    //         LexerError::UnterminatedComment => write!(f, "Unterminated comment"),
-    //     }
-    // }
 }
 
+/// Implementation pour les erreurs et methode de creation
+impl LexerError {
+    pub fn new(kind: LexerErrorKind, message: String, line: usize, column: usize) -> Self {
+        LexerError {
+            kind,
+            message,
+            line,
+            column,
+        }
+    }
+
+    pub fn invalid_character(c: char, line: usize, column: usize) -> Self {
+        Self::new(LexerErrorKind::InvalidCharacter(c),
+                  format!("Invalid character: {}", c), line,column)
+    }
+
+    pub fn invalid_token(t: &str, line: usize, column: usize) -> Self {
+        Self::new(LexerErrorKind::InvalidToken(t.to_string()), format!("Invalid token: {}", t), line, column)
+    }
+    pub fn unterminated_string(line: usize, column: usize) -> Self {
+        Self::new(LexerErrorKind::UnterminatedString, "Unterminated string".to_string(), line, column
+        )
+    }
+    pub fn unterminated_comment(line: usize, column: usize) -> Self {
+        Self::new(LexerErrorKind::UnterminatedComment, "Unterminated comment".to_string(), line, column)
+    }
+}
+
+
+impl From<LexerErrorKind> for LexerError {
+    fn from(kind: LexerErrorKind) -> Self {
+        let message = match &kind {
+            LexerErrorKind::InvalidCharacter(c) => format!("Invalid character: {}", c),
+            LexerErrorKind::InvalidToken(t) => format!("Invalid token: {}", t),
+            LexerErrorKind::UnterminatedString => "Unterminated string".to_string(),
+            LexerErrorKind::UnterminatedComment => "Unterminated comment".to_string(),
+        };
+        LexerError {
+            kind,
+            message,
+            line: 0,  // Ces valeurs devraient être mises à jour par l'appelant
+            column: 0,
+        }
+    }
+}
+
+impl LexerErrorKind {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            LexerErrorKind::InvalidCharacter(_) => "Invalid Character",
+            LexerErrorKind::InvalidToken(_) => "Invalid Token",
+            LexerErrorKind::UnterminatedString => "Unterminated String",
+            LexerErrorKind::UnterminatedComment => "Unterminated Comment",
+        }
+    }
+}
+
+impl Error for LexerError {}
--- src/lexer/lex.rs
+++ src/lexer/lex.rs
@@ -4,10 +4,9 @@ use std::str::Chars;
 use std::iter::Peekable;
 use std::process::exit;
 use std::collections::HashMap;
-use std::error::Error;
 use crate::lexer::token_type::TokenType;
 use crate::lexer::error::LexerError;
-
+use std::error::Error;
 
 
 
@@ -57,20 +56,38 @@ impl <'a>Lexer<'a> {
             keywords:Lexer::init_keyword(),
             current_indent: 0,
         };
+        lexer.next_char();
+        lexer.detect_syntax();
+        lexer
+
 
     }
-    fn detect_syntax(){}
+    fn detect_syntax(&mut self){
+        let mut peek_iter = self.source.clone();
+        while let Some(c) = peek_iter.next(){
+            if !c.is_whitespace(){
+                if c == '{'{
+                    self.syntax_mode = SyntaxMode::Braces;
+                }
+                break;
+            }
+        }
+    }
     fn handle_indentation(){}
     fn init_keywords(){}
 
-    pub fn next_char(){}
-    pub fn peek(){}
+    pub fn next_char(&mut self){
+        self.current_char = self.source.next()
+    }
+    pub fn peek(&mut self) -> Option<&char>{
+        self.source.peek()
+    }
     pub fn abort(){}
     pub fn get_token(){}
     fn get_identifier_or_keyword(){}
     fn get_number(){}
     fn get_string(){}
-    pub fn tokenize(){}
+    pub fn tokenize(&mut self){}
     pub fn skip_whitespace(){}
     pub fn skip_comment(){}
 
